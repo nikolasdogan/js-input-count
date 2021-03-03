e1749a7a21d5c0e73eb2902307b613bd236db1ee
Nikolas Doğan Halil <nikolasdogan@gmail.com>
2021-03-03 20:13:59 +0300

    Update say.js

--- say.js
+++ say.js
@@ -4,3 +4,31 @@
 		mesaj.onkeyup = function(){
 			sayi.innerHTML = 160 - this.value.length;
 		}
+
+
+/////////////////////////////////////////////
+
+#yöntem 2
+
+<script>
+"use strict";
+  var mesaj = document.getElementById("mesaj");
+  var karakter = document.getElementById("karakter");
+
+  var karakterSiniri = 140;
+
+  mesaj.onkeyup = karakterKontrol;
+  mesaj.onkeydown = karakterKontrol;
+
+  function karakterKontrol() {
+    var mesajUzunluk = this.value.length;
+
+    if (karakterSiniri >= mesajUzunluk) {
+      var kalan = karakterSiniri - mesajUzunluk;
+      karakter.innerHTML = kalan + " karakter kaldı";
+
+    } else {
+      this.value = this.value.substr(0, karakterSiniri);
+    }
+  }
+</script>
